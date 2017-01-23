f703a1fb966d486932848e41354292af58432a5e
Stephan Gabler <stephan.gabler@gmail.com>
2017-01-23 14:57:12 +0100

    Run slow tests on export branches.

---
+++ should_run_slow_tests.sh
@@ -0,0 +1,9 @@
+# Determine whether the big test suite with execution of the Makefile and the slow tests should be run.
+# Slow tests will be executed if the branch name contains `export`.
+
+readonly GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+if [[ $GIT_BRANCH =~ "export" ]] ; then
+  exit
+fi
+
+touch skip-slow-tests
